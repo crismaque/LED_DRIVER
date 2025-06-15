faf02bd48a5ddf765a8abac1d065c49a38961197
Gabs <107444708+GabrielaRomoM@users.noreply.github.com>
2025-06-15 10:24:04 -0500

    Update user_app.h

--- src/user_app.h
+++ src/user_app.h
@@ -1,8 +1,9 @@
 #ifndef USER_APP_H
 #define USER_APP_H
 
+// Define la ruta del dispositivo de carácter que va a ser utilizado
 #define DEVICE "/dev/led_gpio"
 
-int main (int argc, char *argv[]);
+int main (int argc, char *argv[]); // Función principal de la aplicación
 
 #endif
